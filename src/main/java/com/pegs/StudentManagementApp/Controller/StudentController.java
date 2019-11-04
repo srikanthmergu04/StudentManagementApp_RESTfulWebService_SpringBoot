3d7385c7d8346cbebc063624c1f292826065f53b
SrikanthMergu <mergusrikanth04@gmail.com>
2019-11-04 11:34:04 +0530

    Implemented logging using logback and added sonar-project.properties file for code analysis

--- src/main/java/com/pegs/StudentManagementApp/Controller/StudentController.java
+++ src/main/java/com/pegs/StudentManagementApp/Controller/StudentController.java
@@ -2,6 +2,8 @@ package com.pegs.StudentManagementApp.Controller;
 
 import com.pegs.StudentManagementApp.Model.Student;
 import com.pegs.StudentManagementApp.Service.StudentServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,32 +16,40 @@ public class StudentController {
     @Autowired
     private StudentServiceImpl studentService;
 
+    Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @PostMapping("/Students")
     public Student addStudent(@RequestBody Student student){
+        logger.trace("addStudent Method in StudentController is accessed.");
         studentService.save(student);
+        logger.trace("Student Obj is saved to Database");
         return student;
     }
 
     @GetMapping("/Students")
     public List<Student> getStudents(){
+        logger.trace("getStudents Method in StudentController is accessed.");
         return studentService.getStudents();
+
 
     }
 
     @GetMapping("/Students/{sid}")
     public Optional<Student> getStudentById(@PathVariable Integer sid){
+        logger.trace("getStudentById method is StudentController is accessed.");
         return studentService.getStudentById(sid);
     }
 
     @PutMapping("Students/{sid}")
     public Student updateStudentById(@PathVariable Integer sid , @RequestBody Student student){
+        logger.trace("updateStudentById method in StudentController is accessed.");
         student.setSid(sid);
         return studentService.updateStudent(student);
     }
 
     @DeleteMapping("/Students/{sid}")
     public String deleteStudentById(@PathVariable Integer sid){
-
+        logger.trace("deleteStudentById method in StudentController is accessed.");
         studentService.deleteStudentById(sid);
         return "Deleted..";
     }
