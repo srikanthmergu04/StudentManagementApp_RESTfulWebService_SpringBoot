eea06423ab9dd572452d3f6142a8706963dc5701
SrikanthMergu <mergusrikanth04@gmail.com>
2019-11-07 14:54:31 +0530

    updated readme.md - added Caching Implementation & Implemented loggin in StudentServiceImpl Class

--- README.md
+++ README.md
@@ -5,7 +5,31 @@ Project : StudentManagementApp
   - H2 - in memory Database.
   - Spring Data JPA - uses Hibernate as JPA implementation.
   - Swagger - used for Documenting Rest API’s.
-  - Logback - used for logging
+  - Logback - used for logging.
+  - Spring-Data-Redis for Caching.
 - It is RESTful Web Service.
 - Implemented CRUD operations.
+- Implemented Caching using Redis.
 - Github link : https://github.com/srikanthmergu04/StudentManagementApp_RESTfulWebService_SpringBoot.git
+
+Cache Implementation using Spring-Data-Redis
+ ---------------------------------------------------
+ - #### Installing Redis on windows 10
+    - Downlad link : https://github.com/microsoftarchive/redis/releases
+        - you can download either .zip or .msi , and extract it
+    
+  - #### Starting Redis Server Locally
+    - open CMD and point to the redis installation directory and execute the below command.
+            
+            `redis-server`
+    - you can see "**server started**" message on console.
+  - #### Cache Configuration in application.properties file
+    - `spring.cache.type=redis`
+    - `spring.redis.host=localhost`
+    -  `spring.redis.port=6379`
+    
+    - you can see `application.properties` file for remaining configurations.
+        
+     
+         
+  
--- src/main/java/com/pegs/StudentManagementApp/Service/StudentServiceImpl.java
+++ src/main/java/com/pegs/StudentManagementApp/Service/StudentServiceImpl.java
@@ -1,7 +1,10 @@
 package com.pegs.StudentManagementApp.Service;
 
+import com.pegs.StudentManagementApp.Controller.StudentController;
 import com.pegs.StudentManagementApp.Dao.StudentDaoImpl;
 import com.pegs.StudentManagementApp.Model.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
@@ -15,21 +18,27 @@ public class StudentServiceImpl {
     @Autowired
     private StudentDaoImpl studentDao;
 
+    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
+
     @Caching(
             put = {@CachePut(cacheNames = "StudentCache" , key = "#student.sid")} ,
             evict = {@CacheEvict(cacheNames = "AllStudentsCache" , allEntries = true)}
     )
     public Student save(Student student) {
+        logger.info("Student Obj saved to Database");
+        logger.info("Student Obj saved to Cache");
         return studentDao.save(student);
     }
 
     @Cacheable(cacheNames = "AllStudentsCache")
     public List<Student> getStudents() {
+        logger.info("Students returned from Database");
         return studentDao.findAll();
     }
 
     @Cacheable(cacheNames = "StudentCache" , key = "#sid")
     public Optional<Student> getStudentById(Integer sid) {
+        logger.info("Student returned from Database");
         return studentDao.findById(sid);
     }
 
@@ -41,6 +50,8 @@ public class StudentServiceImpl {
             }
     )
     public void deleteStudentById(Integer sid) {
+        logger.info("Student Obj is deleted from Database");
+        logger.info("Student Obj is deleted from Cache");
         studentDao.deleteById(sid);
     }
 
@@ -49,6 +60,8 @@ public class StudentServiceImpl {
             evict = {@CacheEvict(cacheNames = "AllStudentsCache" , allEntries = true)}
     )
     public Student updateStudent(Student student) {
+        logger.info("Student Obj in Database updated");
+        logger.info("Student Obj in Cache updated");
         return studentDao.save(student);
     }
 }
